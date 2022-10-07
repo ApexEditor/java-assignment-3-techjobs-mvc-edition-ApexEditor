11dfe1e8bc761c9531ed627c1939ea54ce533bb9
Jerihmy <jhickso1@student.ccp.edu>
2022-10-07 19:43:53 -0400

    Autograder passed

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -29,7 +29,7 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
-    public String displaySearchResults(@RequestParam String searchType, @RequestParam String searchTerm, Model model){
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
        ArrayList<Job> jobs;
        jobs = JobData.findByColumnAndValue(searchType, searchTerm);
        model.addAttribute("searchType", searchType);
@@ -37,7 +37,7 @@ public class SearchController {
        model.addAttribute("columns", columnChoices);
        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
        model.addAttribute("jobs", jobs);
-       return "search-html";
+       return "search.html";
     }
 
 
